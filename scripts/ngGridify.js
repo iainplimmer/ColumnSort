8464e5c9396147299a0bee7e02326f4e07b2a41d
iain plimmer <iainplimmer@iains-iMac.local>
2016-06-10 22:32:24 +0100

    We now have dynamic columns

--- scripts/ngGridify.js
+++ scripts/ngGridify.js
@@ -5,27 +5,22 @@
         .component('ngGridify', {
         bindings: {
             data: '=',
-            order: '='
+            order: '=',
+            columns: '='
         },
         controllerAs: 'ctrl',
         controller: function ($http) {
             var vm = this;
-
             vm.reverse = false;
-
             vm.SortColumn = function SortColumn (column) {
                 vm.order = column;
                 vm.reverse = !vm.reverse;
             }
-
         },
         template: [
             '<table>',
             '<tr>',
-            '<th><a href="#" ng-click="ctrl.SortColumn(\'name\')">Name</a></th>',
-            '<th><a href="#" ng-click="ctrl.SortColumn(\'age\')">Age</a></th>',
-            '<th><a href="#" ng-click="ctrl.SortColumn(\'gender\')">Gender</a></th>',
-            '<th><a href="#" ng-click="ctrl.SortColumn(\'email\')">Email</a></th>',
+            '<th ng-repeat="column in ctrl.columns"><a href="#" ng-click="ctrl.SortColumn(column)">{{column}}</a></th>',
             '</tr>',
             '<tr ng-repeat="person in ctrl.data | orderBy : ctrl.order : ctrl.reverse">',
             '<td>{{person.name}}</td>',
