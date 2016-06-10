9f5d11178b040d2578921b7a805dde9b36c37bd4
iain plimmer <iainplimmer@iains-iMac.local>
2016-06-10 23:04:13 +0100

    We now have a config object that will be used to configure the component.

--- scripts/app.js
+++ scripts/app.js
@@ -6,14 +6,20 @@
         var vm = this;
         vm.$onInit = function () { 
             $http.get('./resources/people.json').then(function success(response){            
-                vm.data = response.data;    
-                vm.order = 'name';    
-                vm.columns = [
-                    { column: 'name', display: 'Name' }, 
-                    { column: 'age', display: 'Age' },
-                    { column: 'gender', display: 'Gender' },
-                    { column: 'email', display: 'Email' },
-                ]      
+
+                //  Let's make a new config object that allows us to just bind the data to columns in one
+                vm.config = {
+                    data : response.data,
+                    order : 'name', 
+                    columns: [
+                        { column: 'name', display: 'Name' }, 
+                        { column: 'age', display: 'Age' },
+                        { column: 'gender', display: 'Gender' },
+                        { column: 'email', display: 'Email' },
+                    ]         
+                };
+
+                  
             });
         };
     })
--- scripts/ngGridify.js
+++ scripts/ngGridify.js
@@ -4,26 +4,24 @@
     angular.module('ngGridify', [])
         .component('ngGridify', {
         bindings: {
-            data: '=',
-            order: '=',
-            columns: '='
+            config: '='
         },
         controllerAs: 'ctrl',
         controller: function ($http) {
             var vm = this;
             vm.reverse = false;
             vm.SortColumn = function SortColumn (column) {
-                vm.order = column;
+                vm.config.order = column;
                 vm.reverse = !vm.reverse;
             }
         },
         template: [
             '<table>',
             '<tr>',
-            '<th ng-repeat="column in ctrl.columns"><a href="#" ng-click="ctrl.SortColumn(column.column)">{{column.display}}</a></th>',
+            '<th ng-repeat="column in ctrl.config.columns"><a href="#" ng-click="ctrl.SortColumn(column.column)">{{column.display}}</a></th>',
             '</tr>',
-            '<tr ng-repeat="item in ctrl.data | orderBy : ctrl.order : ctrl.reverse">',
-            '<td ng-repeat="column in ctrl.columns">{{item[column.column]}}</td>',
+            '<tr ng-repeat="item in ctrl.config.data | orderBy : ctrl.config.order : ctrl.reverse">',
+            '<td ng-repeat="column in ctrl.config.columns">{{item[column.column]}}</td>',
             '</tr>',
             '</table>'
         ].join('')
